021cf8929666ef5745ae9ace0a6f3aecf667397c
Marcelo Augusto Coldibelli <marcelo.coldibelli@gmail.com>
2024-11-02 06:56:35 -0300

    feat: add planet repository

---
+++ starwars/src/main/java/online/cotefacil/starwars/repository/PlanetRepository.java
@@ -0,0 +1,9 @@
+package online.cotefacil.starwars.repository;
+
+import online.cotefacil.starwars.model.Planet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanetRepository extends JpaRepository<Planet, Long> {
+}
