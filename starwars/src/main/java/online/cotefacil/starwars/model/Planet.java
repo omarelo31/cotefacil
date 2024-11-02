feaa49715fc9c723d7a965842f84314ae9fcded6
Marcelo Augusto Coldibelli <marcelo.coldibelli@gmail.com>
2024-11-02 06:47:25 -0300

    feat: add planet model

---
+++ starwars/src/main/java/online/cotefacil/starwars/model/Planet.java
@@ -0,0 +1,24 @@
+package online.cotefacil.starwars.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "planets")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Planet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String climate;
+    private String terrain;
+    private Long appearance;
+}
